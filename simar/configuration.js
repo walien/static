9874bcc82dae1da75984beed44346fd9fcfdda72
Elian ORIOU <walien@users.noreply.github.com>
2016-03-24 18:38:12 +0100

    Update configuration.js

--- simar/configuration.js
+++ simar/configuration.js
@@ -1,14 +1,14 @@
+var preprod = 'http://www.preprod.simar.re/mobile';
+var prod = 'https://www.simar.re/mobile';
+var localhost  = 'http://localhost:8082';
+
 angular.module('config', [])
     .constant('ServerConfig', {
-
-        var preprod = 'http://www.preprod.simar.re/mobile';
-        var prod = 'https://www.simar.re/mobile';
-        var localhost  = 'http://localhost:8082';
          
-        var configuration = {
+        configuration: {
             'dev': localhost,
             '1.0': preprod
-        };
+        },
 
         backendUrl: function () {
             var url = configuration[appVersion];
