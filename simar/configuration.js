05de125e8cdcbbced760edb39a344fd1c1af2c89
Elian ORIOU <elian.oriou@4sh.fr>
2016-03-24 19:21:11 +0100

    update server config file

--- simar/configuration.js
+++ simar/configuration.js
@@ -1,19 +1,28 @@
-var preprod = 'http://www.preprod.simar.re/mobile';
-var prod = _default = 'https://www.simar.re/mobile';
-var localhost  = '';
+(function (module) {
 
-var configuration = {
-    'dev': localhost,
-    '1.0': preprod
-};
+    var Envs = {
+        preprod: 'http://www.preprod.simar.re/mobile',
+        prod: 'https://www.simar.re/mobile',
+        local: ''
+    };
+    Envs._default = Envs.local;
 
-angular.module('config', [])
-    .constant('ServerConfig', {
-
+    var ServerConfig = {
         backendUrl: function () {
             var url = configuration[appVersion];
-            if (url === undefined || url === null) return _default;
+            if (url === undefined || url === null) return Envs._default;
             return url;
         }
+    };
 
-    });
+    var configuration = {
+        'dev': Envs.local,
+        '1.0': Envs.preprod
+    };
+
+    module
+        .constant('ServerConfig', ServerConfig)
+        .constant('ServerUrl', ServerConfig.backendUrl())
+        .constant('ServerEnvs', Envs);
+
+})(angular.module('config', []));
